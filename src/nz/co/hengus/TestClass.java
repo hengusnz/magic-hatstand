af05dbf65023c09e83ff3d35dd7b588a3176517a
martin <martin@hengus.co.nz>
2015-10-04 16:52:59 +1300

    First commit!

---
+++ src/nz/co/hengus/TestClass.java
@@ -0,0 +1,10 @@
+package nz.co.hengus;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
---
+++ src/nz/co/hengus/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author martin
+ *
+ */
+package nz.co.hengus;
